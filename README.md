237d8e7f246fcc2c345cedf376be2827f4066ec5
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-01-31 10:46:18 +0530

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# RealTime Chat-App using NODEJS
+
+## Run application
+
+1.  Using node
+
+        npm start
+
+    NODE.JS mst be installed to run this application as this depends on the JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.
+
+2.  Using nodemon
+
+        npm run dev
+
+    NODE.JS mst be installed to run this application as this depends on the JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.
+
+## Working example
+
+[![Working example](https://drive.google.com/file/d/1NM_xIJYG_oJq5CBTMf_exnABCaD_PDH6/view)](https://drive.google.com/file/d/1iC7Pjsmn1wfMkhU0VUYgVB_y_tjRv6H7/view?usp=sharing)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Config the application dependancies
 const path = require('path')
 const http = require('http')
 const express = require('express')
@@ -6,6 +7,7 @@ const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
+// Congif application start 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -13,11 +15,13 @@ const io = socketio(server)
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Configure static files
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
+    // On join call
     socket.on('join', (options, callback) => {
         const { error, user } = addUser({ id: socket.id, ...options })
 
@@ -25,6 +29,7 @@ io.on('connection', (socket) => {
             return callback(error)
         }
 
+        // Join user
         socket.join(user.room)
 
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
@@ -37,6 +42,7 @@ io.on('connection', (socket) => {
         callback()
     })
 
+    // On send message
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
         const filter = new Filter()
@@ -45,16 +51,19 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!')
         }
 
+        // Emit message with username attached
         io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
+    // On sedn location with username attached
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id)
         io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
+    // Disconnect from server and sockets.
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
 
